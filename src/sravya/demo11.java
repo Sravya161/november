2d7c2b2a9cfddf143327a0522a4e1a00d86731a1
DELL <DELL@192.168.10.45>
2023-11-03 09:25:58 +0530

    update program

--- src/sravya/demo11.java
+++ src/sravya/demo11.java
@@ -5,6 +5,9 @@ public class demo11 {
 	public static void main(String[] args) {
 
 		System.out.println("sravya");
+		System.out.println("Marolix technology");
+		System.out.println("Hyderabad");
+		System.out.println("MT-01322");
 	}
 
 }
