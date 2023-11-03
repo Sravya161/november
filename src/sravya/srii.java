c95abdc052f320221e5cad272d8d39761836d605
DELL <DELL@192.168.10.45>
2023-11-03 09:43:38 +0530

    updated

---
+++ src/sravya/srii.java
@@ -0,0 +1,11 @@
+package sravya;
+
+public class srii {
+
+	public static void main(String[] args) {
+		System.out.println("Marolix technology");
+		System.out.println("Hyderabad");
+		System.out.println("MT-01322");
+	}
+
+}
